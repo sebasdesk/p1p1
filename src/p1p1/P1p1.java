9089e150bb52e4c51df8173f5299645e3a60578b
sebas <sebas@172.16.100.198>
2016-07-26 20:36:28 -0500

    version 1.0 ejercicio 1

---
+++ src/p1p1/P1p1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p1p1;
+
+/**
+ *
+ * @author sebas
+ */
+public class P1p1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola github v1.0");
+        // TODO code application logic here
+    }
+    
+}
